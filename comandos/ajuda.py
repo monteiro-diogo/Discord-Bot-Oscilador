84d34a0de19862d4bd8bbef747c031c191133efc
t-ros <diogo.mm.monteiro@gmail.com>
2025-04-15 17:50:49 +0100

    update: new embed !ajuda

--- comandos/ajuda.py
+++ comandos/ajuda.py
@@ -5,15 +5,27 @@ class Ajuda(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="ajuda", help="``Mostra a lista de comandos disponíveis.``")
+    @commands.command(name="ajuda", aliases=["h"], help="Mostra a lista de comandos disponíveis.")
     async def ajuda(self, ctx):
         prefixo = (await self.bot.get_prefix(ctx.message))[0]
-        comandos = ""
-        for command in self.bot.commands:
-            comandos += f"{prefixo}{command.name} {command.help or 'Sem descrição'}\n"
 
-        await ctx.send(f"=== **Lista de comandos disponíveis** ===\n{comandos}============================")
+        # Cria o embed
+        embed = discord.Embed(
+            title="**Lista de Comandos**",
+            description=f"Aqui estão os comandos disponíveis no bot. Use-os com o prefixo `{prefixo}`.",
+            color=discord.Color.blue()  # Escolha a cor que preferir
+        )
+
+        # Enumera os comandos
+        comandos = ""
+        for i, command in enumerate(self.bot.commands, 1):
+            # Cada comando é listado com um índice e uma breve descrição
+            comandos += f"**{i}.** `{prefixo}{command.name}` - {command.help or 'Sem descrição'}\n"
+        
+        embed.add_field(name="Comandos:", value=comandos, inline=False)
+
+        # Envia o embed
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Ajuda(bot))
-
